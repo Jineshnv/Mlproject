6daf41e765844c397f30def77fdf0a2dc91d84dc
Jineshnv <jineshnv123@gmail.com>
2023-03-19 22:26:50 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hypen_E_dot='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+        '''
+        This function will return list
+        '''
+        requirements=[]
+        with open (file_path) as file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace("\n","") for req in requirements]
+
+            if Hypen_E_dot in requirements:
+                requirements.remove(Hypen_E_dot)
+
+        return requirements
+setup(
+
+name='mlproject',
+
+version='0.0.1',
+
+author = 'jinesh',
+author_email='jineshnv123@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+
+)
